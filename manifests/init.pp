27affa779ae802cee734b8457d684a3cd7f6e6e8
Tom Booth <tombooth@gmail.com>
2013-09-04 17:39:47 +0100

    [#524141] Upstart conf, only when upstart

--- manifests/init.pp
+++ manifests/init.pp
@@ -96,12 +96,14 @@ class mongodb (
     require => Package['mongodb-10gen'],
   }
 
-  file {'/etc/init/mongodb.conf':
-    source => 'puppet:///modules/mongodb/etc/init/mongodb.conf',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    notify => Service['mongodb'],
+  if $init == 'upstart' {
+    file {'/etc/init/mongodb.conf':
+      source => 'puppet:///modules/mongodb/etc/init/mongodb.conf',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      notify => Service['mongodb'],
+    }
   }
 
   service { 'mongodb':
