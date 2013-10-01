6070508b4f48c4c0973117461626ddd5250dd9d7
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2013-10-01 11:01:54 +0100

    Creates user creation scripts to be loaded into Mongo
    These scripts will be used to ensure users are created for given
    databases and, when requested, made absent.

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,13 +1,28 @@
 define mongodb::user (
   $password,
+  $roles       = '[]',
+  $db_name     = 'test',
   $js_dir      = '/root/puppetlabs-mongodb',
+  $ensure      = 'present'
   ) {
 
-  file { $js_dir:
+  file { "${js_dir}":
     ensure => directory,
-    path   => $js_dir,
+    path   => "${js_dir}",
     owner  => 'root',
     group  => 'root',
     mode   => '0700',
+  }
+
+  $mongodb_script_user = "mongo_user-${title}_${db_name}.js"
+
+  file { "${mongodb_script_user}":
+    ensure  => present,
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+    path    => "${js_dir}/${mongodb_script_user}",
+    content => template('mongodb/user.js.erb'),
+    require => File["${js_dir}"],
   }
 }
--- spec/defines/mongodb_user_spec.rb
+++ spec/defines/mongodb_user_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe 'mongodb::user', :type => :define do
+  let(:password) { 'some_password' }
+
   context "user scripts directory" do
     describe "creates the default directory location" do
       let(:title) { 'creates directory for user js scripts' }
       let(:params) {{
-        :password => "some_password",
+        :password => password,
       }}
 
       it {
@@ -24,7 +26,7 @@ describe 'mongodb::user', :type => :define do
       let(:title) { 'creates directory for user js scripts' }
       let(:path) { '/some_new_path' }
       let(:params) {{
-        :password => "some_password",
+        :password => password,
         :js_dir   => path,
       }}
 
@@ -35,6 +37,50 @@ describe 'mongodb::user', :type => :define do
                  'owner'  => 'root',
                  'group'  => 'root',
                  'mode'   => '0700')
+      }
+    end
+  end
+
+  context "user script create file for user" do
+    describe "creates a default user create script for the 'test' database" do
+      let(:title) { 'joe_bloggs' }
+      let(:file) { "mongo_user-#{title}_test.js" }
+      let(:path) { '/root/puppetlabs-mongodb' }
+      let(:content) { "// File created by Puppet\ndb.addUser(\"#{title}\", \"#{password}\", []);\n" }
+      let(:params) {{
+        :password => password
+      }}
+
+      it {
+        should contain_file(file).with(
+                 'ensure'  => 'present',
+                 'path'    => "#{path}/#{file}",
+                 'owner'   => 'root',
+                 'group'   => 'root',
+                 'mode'    => '0600',
+                 'content' => content)
+      }
+    end
+
+    describe "creates a user js file for the 'some_db_i_haz' database" do
+      let(:title) { 'joe_bloggs' }
+      let(:database) { 'some_db_i_haz' }
+      let(:file) { "mongo_user-#{title}_#{database}.js" }
+      let(:path) { '/root/puppetlabs-mongodb' }
+      let(:content) { "// File created by Puppet\ndb.addUser(\"#{title}\", \"#{password}\", []);\n" }
+      let(:params) {{
+        :password => password,
+        :db_name  => database
+      }}
+
+      it {
+        should contain_file(file).with(
+                 'ensure'  => 'present',
+                 'path'    => "#{path}/#{file}",
+                 'owner'   => 'root',
+                 'group'   => 'root',
+                 'mode'    => '0600',
+                 'content' => content)
       }
     end
   end
